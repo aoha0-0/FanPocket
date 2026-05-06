5fe6d4cf6304c895ee6a42a5bac4a199f6db4773
aoha0-0 <aquarium.swing@gmail.com>
2026-05-06 14:59:48 +0900

    ログイン・サインアップ後のリダイレクト先設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
-  # ログイン済ユーザーのみにアクセスを許可する
   before_action :authenticate_user!
+
+  private
+
+  # ログイン後のリダイレクト先を指定
+  def after_sign_in_path_for(resource)
+    root_path 
+
+  # サインアップ後のリダイレクト先を指定
+  def after_sign_up_path_for(resource)
+    root_path
+  end
 end
