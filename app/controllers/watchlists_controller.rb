34c5029e30102e47186821b309622bfa7b1a5ff6
aoha0-0 <aquarium.swing@gmail.com>
2026-05-22 06:01:18 +0900

    ステータスの切り替え処理、予定を通知対象から除外するロジックの準備

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -2,10 +2,20 @@ class WatchlistsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_watchlist, only: [:show, :edit, :update]
 
-  def index
-    @watchlists =current_user.watchlists.order(start_at: :asc, end_at: :asc)
-  end
+ def index
+    # 1. 「これからの予定」：まだ完了していなくて（is_done: false）、終了日時が未来、または未設定のもの
+    @future_watchlists = current_user.watchlists
+                                     .where(is_done: false)
+                                     .where("end_at >= ? OR end_at IS NULL", Time.current)
+                                     .order(start_at: :asc)
 
+    # 2. 「これまでの足跡」：すでに完了している（is_done: true）か、または終了日時が過去のもの
+    @past_watchlists = current_user.watchlists
+                                   .where(is_done: true)
+                                   .or(current_user.watchlists.where("end_at < ?", Time.current))
+                                   .order(end_at: :desc)
+  end
+  
   def show
   end
 
@@ -61,6 +71,6 @@ class WatchlistsController < ApplicationController
   end
 
   def watchlist_params
-    params.require(:watchlist).permit(:title, :memo, :url, :start_at, :end_at, :is_done, :image)
+    params.require(:watchlist).permit(:title, :memo, :url, :start_at, :end_at, :is_done)
   end
 end
