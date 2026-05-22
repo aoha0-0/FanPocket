3bec221cb85bb9342787283f4a2f41a281f0b728
aoha0-0 <aquarium.swing@gmail.com>
2026-05-23 02:43:16 +0900

    developmentとproductionのメール設定を分離しリマインドメールの土台を作成

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,2 +1,13 @@
 class NotificationMailer < ApplicationMailer
+  default from: 'noreply@fanpocket.fun'
+
+  def send_notice(user_email, title, content)
+    @title = title
+    @content = content
+
+    mail(
+      to: user_email,
+      subject: "【FanPocket】新しいお知らせ: #{@title}"
+    )
+  end
 end
