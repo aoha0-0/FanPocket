73041342e732573d3123d415569063afcad8ac38
aoha0-0 <aquarium.swing@gmail.com>
2026-05-24 23:12:07 +0900

    3日前・前日・当日の通知メソッドを分離し当日メールのボタンをコーラルオレンジに変更

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -10,4 +10,25 @@ class NotificationMailer < ApplicationMailer
       subject: "【FanPocket】新しいお知らせ: #{@title}"
     )
   end
+
+  # 3日前通知
+  def three_days_ago_notice(user_email, title, content)
+    @title = title
+    @content = content
+    mail(to: user_email, subject: "【FanPocket】あと3日で締切：#{@title}")
+  end
+
+  # 前日20時通知
+  def day_before_notice(user_email, title, content)
+    @title = title
+    @content = content
+    mail(to: user_email, subject: "【重要】明日締切：#{@title}")
+  end
+
+  # 当日7時通知
+  def today_notice(user_email, title, content)
+    @title = title
+    @content = content
+    mail(to: user_email, subject: "【本日締切】お忘れではありませんか？：#{@title}")
+  end
 end
