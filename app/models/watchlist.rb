532e773d4d844c72bc7794e252b0cfebd9673673
aoha0-0 <aquarium.swing@gmail.com>
2026-06-06 14:36:04 +0900

    締切日時のみ未来の日時を必須とするようにバリデーションを修正

--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -7,7 +7,7 @@ class Watchlist < ApplicationRecord
   VALID_URL_REGEX = /\Ahttps?:\/\/[\S]+\z/
   validates :url, allow_blank: true, format: { with: VALID_URL_REGEX }
 
-  validate :end_at_must_be_after_start_at
+  validate :end_at_must_be_future
   
   #3日前通知用
   scope :alert_three_days_prior, -> {
@@ -54,6 +54,12 @@ class Watchlist < ApplicationRecord
   def end_at_must_be_after_start_at
     if start_at.present? && end_at.present? && end_at <= start_at
       errors.add(:end_at, :must_be_after_start_at)
+    end
+  end
+
+  def end_at_must_be_future
+    if end_at.present? && end_at < Time.current
+    errors.add(:end_at, "は未来の日時を選択してください")
     end
   end
 end
