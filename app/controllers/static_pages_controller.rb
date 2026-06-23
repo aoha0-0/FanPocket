1901b577a4fc904c17e77eaaeb2c72fd555b031d
aoha0-0 <aquarium.swing@gmail.com>
2026-06-23 21:59:55 +0900

    feat: フッターと設定画面に利用規約・プライバシーポリシーのリンクを追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class StaticPagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:terms, :privacy]
+  skip_before_action :authenticate_user!, only: %i[terms privacy]
 
   def terms; end
 
