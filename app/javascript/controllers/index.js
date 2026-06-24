77230ee742959cb3456c706c67e4151b15467dcc
aoha0-0 <aquarium.swing@gmail.com>
2026-06-24 19:00:46 +0900

    fix: index.jsのapplicationインポートパスをImportmapの指定に修正

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,7 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "controllers/application"
 
 // Importmap環境でコントローラーを正しく自動ロードする記述
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
