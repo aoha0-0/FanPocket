7fb4e54dc2bc7c6af3bfb776259ac89ae68aad28
aoha0-0 <aquarium.swing@gmail.com>
2026-08-03 02:34:18 +0900

    GitHub Actions・Sidekiq・Redisを削除

--- config/initializers/sidekiq.rb
+++
@@ -1,8 +0,0 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL") }
-  config.average_scheduled_poll_interval = 300
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL") }
-end
