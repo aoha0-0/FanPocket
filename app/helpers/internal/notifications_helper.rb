c5dcbf7a14ca18f65081e86e9e4e22df738115fa
aoha0-0 <aquarium.swing@gmail.com>
2026-08-03 01:51:20 +0900

    GASから通知処理を実行するエンドポイントを追加

---
+++ app/helpers/internal/notifications_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Internal
+  module NotificationsHelper
+  end
+end
---
+++ test/controllers/internal/notifications_controller_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Internal
+  class NotificationsControllerTest < ActionDispatch::IntegrationTest
+    # test "the truth" do
+    #   assert true
+    # end
+  end
+end
