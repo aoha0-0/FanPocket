1e026002386d785b314d68a689d2e8c76dc46f06
aoha0-0 <aquarium.swing@gmail.com>
2026-08-22 03:18:54 +0900

    メール文面にFanPocketらしい絵文字を追加

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -5,34 +5,34 @@ class NotificationMailer < ApplicationMailer
     @title = title
     @content = content
 
-    mail(to: user_email, subject: "【FanPocket】新しいお知らせ: #{@title}")
+    mail(to: user_email, subject: "【FanPocket】🌟新しいお知らせ：#{@title}")
   end
 
   # 3日前通知
   def three_days_ago_notice(user_email, title, content)
     @title = title
     @content = content
-    mail(to: user_email, subject: "【FanPocket】あと3日で締切です：#{@title}")
+    mail(to: user_email, subject: "【FanPocket】📅あと3日で締切です：#{@title}")
   end
 
   # 前日20時通知
   def day_before_notice(user_email, title, content)
     @title = title
     @content = content
-    mail(to: user_email, subject: "【FanPocket】明日締切です：#{@title}")
+    mail(to: user_email, subject: "【FanPocket】🌟明日締切です：#{@title}")
   end
 
   # 当日7時通知
   def today_notice(user_email, title, content)
     @title = title
     @content = content
-    mail(to: user_email, subject: "【FanPocket】本日締切です：#{@title}")
+    mail(to: user_email, subject: "【FanPocket】⏰本日締切です：#{@title}")
   end
 
   def start_notice(user_email, title, content)
     @title = title
     @content = content
 
-    mail(to: user_email, subject: "【FanPocket】本日開始です：#{@title}")
+    mail(to: user_email, subject: "【FanPocket】✨本日開始です：#{@title}")
   end
 end
