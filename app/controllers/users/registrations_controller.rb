012dd89898a3a7fc9406cc543726756dd31ff669
aoha0-0 <aquarium.swing@gmail.com>
2026-09-04 19:09:50 +0900

    アカウント削除機能を追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -29,6 +29,10 @@ module Users
       end
     end
 
+    def after_sign_out_path_for(_resource_or_scope)
+      new_user_session_path
+    end
+
     def after_update_path_for(_resource)
       settings_path
     end
